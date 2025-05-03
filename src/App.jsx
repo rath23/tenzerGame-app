2204729dc8026b0cdd2c2013b8e6a4495fe26f8b
rath23 <muneer3@amityonline.com>
2025-05-03 16:16:40 +0530

    made miner changes in ui

--- src/App.jsx
+++ src/App.jsx
@@ -43,6 +43,10 @@ function App() {
   return (
     <>
     {gameWon && <Confetti />}
+    <h1 className="title">Tenzies</h1>
+    <p className="instructions">
+      Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
+    </p>
     <div className="dice-wrapper">
       {values.map((value, index) => (
         <div key={index} className="dice-container" >
